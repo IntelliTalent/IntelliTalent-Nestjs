abada46d811f5e26f21436395fbe4a5af89b1e8d
BeshoyMorad <besh0morta@gmail.com>
2024-03-09 14:30:23 +0200

    Create all message patterns for jobs service

--- apps/jobs/src/main.ts
+++ apps/jobs/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { JobsModule } from './jobs.module';
+import {
+  ServiceName,
+  SharedService,
+  mapServiceNameToQueueName,
+} from '@app/shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(JobsModule);
+
+  const sharedService = app.get(SharedService);
+
+  app.connectMicroservice(
+    await sharedService.getRmqOptions(
+      await mapServiceNameToQueueName(ServiceName.JOB_SERVICE),
+    ),
+  );
 
   app.startAllMicroservices();
   console.log('Jobs service is running');
