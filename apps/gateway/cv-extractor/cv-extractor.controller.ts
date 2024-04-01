ab9841cca7964ff932f4ba5a93e3d26f7c7b71fc
BeshoyMorad <besh0morta@gmail.com>
2024-04-01 20:56:35 +0200

    Create endpoint to upload and download the cv

--- apps/gateway/cv-extractor/cv-extractor.controller.ts
+++ apps/gateway/cv-extractor/cv-extractor.controller.ts
@@ -1,12 +1,25 @@
 import {
-  CvExtractInfoDto,
   cvExtractorPattern,
   ICvInfo,
 } from '@app/services_communications/cv-extractor-service';
 import { ServiceName } from '@app/shared';
-import { Controller, Post, Inject, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Inject,
+  UseInterceptors,
+  UploadedFile,
+  Get,
+  StreamableFile,
+  Param,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { join } from 'path';
+import { diskStorage } from 'multer';
+import { Public } from '@app/shared/decorators/ispublic-decorator.decorator';
+import { createReadStream } from 'fs';
 
 @ApiTags('CV Extractor')
 @Controller('cv')
@@ -16,6 +29,8 @@ export class CvExtractorController {
     private cvExtractorService: ClientProxy,
   ) {}
 
+  // TODO: Remove public
+  @Public()
   @Post('/info')
   @ApiOperation({ summary: 'Extract the info from a given cv' })
   @ApiResponse({
@@ -23,11 +38,39 @@ export class CvExtractorController {
     type: ICvInfo,
     description: 'Info extracted successfully',
   })
-  async extractInfo(@Body() cv: CvExtractInfoDto) {
-    // throw new NotImplementedException();
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (_, __, callback) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+
+          return callback(null, `${randomName}`);
+        },
+      }),
+    }),
+  )
+  async extractInfo(@UploadedFile() file) {
+    console.log('Going to flask');
+
     return this.cvExtractorService.send(
       { cmd: cvExtractorPattern.extractInfo },
-      cv,
+      { fileId: file.filename },
     );
+  }
+
+  // TODO: Remove public
+  @Public()
+  @Get('/:fileId')
+  getFile(@Param('fileId') fileId: string) {
+    const file = createReadStream(
+      join(process.cwd(), `/uploads/${fileId}.pdf`),
+    );
+    console.log('Some one is here !');
+
+    return new StreamableFile(file);
   }
 }
--- libs/services_communications/src/cv-extractor-service/index.ts
+++ libs/services_communications/src/cv-extractor-service/index.ts
@@ -1,3 +1,2 @@
-export * from './dtos/cv-extract-info.dto';
 export * from './dtos/cv-extracted-info.dto';
 export * from './patterns/cv-extractor.pattern';
