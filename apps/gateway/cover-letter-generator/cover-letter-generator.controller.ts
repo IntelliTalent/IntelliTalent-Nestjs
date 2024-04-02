4bc79376f23889b7388236d2070182b48a27d53a
MoazHassan2022 <moaz25jan2015@gmail.com>
2024-04-02 15:56:37 +0200

    mocked sending to cover pattern

--- apps/gateway/cover-letter-generator/cover-letter-generator.controller.ts
+++ apps/gateway/cover-letter-generator/cover-letter-generator.controller.ts
@@ -1,6 +1,7 @@
 import { GenerateCoverLetterDto } from '@app/services_communications';
 import { CoverLetterResponseDto, coverLetterGeneratorServicePattern } from '@app/services_communications/cover-letter-generator-service';
 import { ServiceName } from '@app/shared';
+import { Public } from '@app/shared/decorators/ispublic-decorator.decorator';
 import { Body, Controller, Header, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -25,18 +26,48 @@ export class ApiCoverLetterGeneratorController {
       description: 'The cover letter links and content',
       type: CoverLetterResponseDto,
   })
+  @Public()
   @Header('content-type', 'application/json')
   @Post('/:profileId')
   async generate(
     @Param('profileId') profileId: string,
     @Body() generateCoverLetterDto: GenerateCoverLetterDto,
   ) {
+    const firstName = "Moaz";
+    const lastName = "Mohammed";
+
+    const startDate = new Date(2022, 1, 1);
+    const endDate = new Date(2024, 1, 1);
+    var companyExperienceYears = endDate.getFullYear() - startDate.getFullYear();
+
+    companyExperienceYears = companyExperienceYears > 1 ? companyExperienceYears : 1;
+
     return this.coverLetterGeneratorService.send(
       {
         cmd: coverLetterGeneratorServicePattern.generate,
       },
       {
-        profileId,
+        // TODO: get data from profile and user
+        profile: {
+          fullName: firstName + " " + lastName,
+          email: "moaz25jan2015@gmail.com",
+          address: "Cairo, Egypt",
+          phoneNumber: "+201111111111",
+          yearsOfExperience: 5,
+          experiences: [
+            {
+              jobTitle: "Software Engineer",
+              companyName: "Intelli Talent",
+              companyExperienceYears,
+            },
+            {
+              jobTitle: "Backend Engineer",
+              companyName: "Google",
+              companyExperienceYears: companyExperienceYears + 2,
+            }
+          ],
+          skills: ["Python", "Flask", "NodeJS", "ExpressJS"],
+        },
         jobTitle: generateCoverLetterDto.jobTitle,
         companyName: generateCoverLetterDto.companyName,
       },
