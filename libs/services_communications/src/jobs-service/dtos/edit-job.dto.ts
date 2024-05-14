12e38f92462ef6e8b55690c4a6f01fa7de6e2e46
BeshoyMorad <besh0morta@gmail.com>
2024-05-14 12:22:45 +0300

    Add userId to edit job dto

--- libs/services_communications/src/jobs-service/dtos/edit-job.dto.ts
+++ libs/services_communications/src/jobs-service/dtos/edit-job.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateJobDto } from './create-job.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class EditJobDto extends CreateJobDto {
   jobId?: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   userId: string;
 }
