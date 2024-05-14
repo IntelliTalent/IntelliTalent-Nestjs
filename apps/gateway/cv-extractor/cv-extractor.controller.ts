a7cb19ebdcad746202c56b86b96366f850fc16cc
BeshoyMorad <besh0morta@gmail.com>
2024-05-14 11:53:04 +0300

    Refactor cv extractor

--- apps/gateway/cv-extractor/cv-extractor.controller.ts
+++ apps/gateway/cv-extractor/cv-extractor.controller.ts
@@ -1,104 +1,50 @@
+import { ICvInfo } from '@app/services_communications/cv-extractor-service';
+import { CurrentUser, Roles, User, UserType } from '@app/shared';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import {
-  cvExtractorPattern,
-  ICvInfo,
-} from '@app/services_communications/cv-extractor-service';
-import { ServiceName } from '@app/shared';
-import {
-  Controller,
-  Post,
-  Inject,
-  UseInterceptors,
-  UploadedFile,
-  Get,
-  Param,
-  Res,
-} from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { join } from 'path';
-import { diskStorage } from 'multer';
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
+import { TriggerExtractInfoDto } from '@app/services_communications/cv-extractor-service/dtos/trigger-extract-info.dto';
+import { AUTH_HEADER } from '@app/services_communications';
+import { CvExtractorService } from './cv-extractor.service';
 import { Public } from '@app/shared/decorators/ispublic-decorator.decorator';
-import { createReadStream } from 'fs';
-import { firstValueFrom } from 'rxjs';
 
 @ApiTags('CV Extractor')
 @Controller('cv')
 export class CvExtractorController {
-  constructor(
-    @Inject(ServiceName.CV_EXTRACTOR_SERVICE)
-    private cvExtractorService: ClientProxy,
-  ) {}
+  constructor(private readonly cvExtractorService: CvExtractorService) {}
 
-  // TODO: Remove public
-  @Public()
   @Post('/info')
+  @Roles([UserType.jobSeeker])
   @ApiOperation({ summary: 'Trigger the info extraction process' })
   @ApiResponse({
     status: 200,
     description: 'Data is currently being processed.',
   })
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (_, __, callback) => {
-          const randomName = Array(32)
-            .fill(null)
-            .map(() => Math.round(Math.random() * 16).toString(16))
-            .join('');
-
-          return callback(null, `${randomName}.pdf`);
-        },
-      }),
-    }),
-  )
-  async triggerExtractInfo(@UploadedFile() file) {
-    // TODO: Save the cv link to the user's profile
-
-    this.saveInfo(file.filename.split('.')[0]);
-
-    return 'Data is currently being processed.';
+  @ApiBearerAuth(AUTH_HEADER)
+  async triggerExtractInfo(
+    @CurrentUser() user: User,
+    @Body() payload: TriggerExtractInfoDto,
+  ) {
+    return this.cvExtractorService.trigger(user.id, payload.cvLink);
   }
 
-  private async saveInfo(filename: string) {
-    const data = await firstValueFrom(
-      this.cvExtractorService.send(
-        { cmd: cvExtractorPattern.extractInfo },
-        { fileId: filename },
-      ),
-    );
-
-    // TODO: Add the data to redis
-  }
-
-  @Public()
   @Get('/extract')
+  @Public()
+  // @Roles([UserType.jobSeeker])
   @ApiOperation({ summary: 'Extract the info from a given cv' })
   @ApiResponse({
     status: 200,
     type: ICvInfo,
     description: 'Info extracted successfully',
   })
+  @ApiBearerAuth(AUTH_HEADER)
+  // async extractInfo(@CurrentUser() user: User) {
   async extractInfo() {
-    // TODO: Get the data from redis and send it back to the user
-  }
-
-  // TODO: Remove public
-  @Public()
-  @Get('/:fileId')
-  getFile(@Param('fileId') fileId: string, @Res() res) {
-    const filePath = join(process.cwd(), `/uploads/${fileId}.pdf`);
-
-    // Set Content-Type header
-    res.set('Content-Type', 'application/pdf');
-
-    // Set Content-Disposition header to force download
-    // TODO: Change the file name to user's name
-    res.set('Content-Disposition', `attachment; filename="${fileId}.pdf"`);
-
-    const fileStream = createReadStream(filePath);
-
-    fileStream.pipe(res);
+    // return this.cvExtractorService.extractInfo(user.id);
+    return this.cvExtractorService.extractInfo('1');
   }
 }
--- libs/services_communications/src/cv-extractor-service/dtos/cv-extracted-info.dto.ts
+++ libs/services_communications/src/cv-extractor-service/dtos/cv-extracted-info.dto.ts
@@ -1,10 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ICvInfo {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty({ type: [String] })
   education: string[];
+
+  @ApiProperty({ type: [String] })
   languages: string[];
+
+  @ApiProperty({ type: [String] })
   skills: string[];
+
+  @ApiProperty({ type: Map })
   projectSkills: Map<string, number>;
+
+  @ApiProperty()
   yearsOfExperience: number;
+
+  @ApiProperty({ type: [String] })
   certifications: string[];
 }
