0f5d8efbd0ccb70ff98d6328996c8db294c49cec
mabdelfattahfahmi <mohamed.fahmy00@eng-st.cu.edu.eg>
2024-06-13 04:27:37 +0300

    resolved last comments

--- apps/gateway/custom-job/custom-job.controller.ts
+++ apps/gateway/custom-job/custom-job.controller.ts
@@ -1,9 +1,10 @@
+import { AUTH_HEADER } from "@app/services_communications";
 import { CustomJobPatterns } from "@app/services_communications/custom-job";
 import { CreateCustomJobDto } from "@app/services_communications/custom-job/dtos/create-custom-job.dto";
 import { Roles, ServiceName, UserType } from "@app/shared";
-import { Body, Controller,  Inject, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @ApiTags('Custom Job')
 @Controller('custom-job')
@@ -14,7 +15,14 @@ export class CustomJobController {
 
     @Post()
     @Roles([UserType.recruiter])
-    async getCustomJobs(
+    @ApiBearerAuth(AUTH_HEADER)
+    @ApiBody({ type: CreateCustomJobDto, description: 'custom job prompt' })
+    @ApiOperation({ summary: 'extract structured job from the prompt' })
+    @ApiResponse({
+        status: 200,
+        description: 'extracted structured job from the prompt successfully',
+    })
+    async createCustomJob(
         @Body() createCustomJobDto: CreateCustomJobDto,
     ) {
         return this.customJobService.send({ cmd: CustomJobPatterns.createCustomJob }, createCustomJobDto);
