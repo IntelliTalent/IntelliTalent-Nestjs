6dde32af5edac344aef44814a56feab558c965b6
waer1 <elwaeryousef@gmail.com>
2024-07-02 00:14:29 +0300

    active emails

--- apps/notifier/src/notifier.service.ts
+++ apps/notifier/src/notifier.service.ts
@@ -32,14 +32,14 @@ export class NotifierService {
   }
 
   async sendMail(email: IEmail) {
-    console.log('sending email', email);
+    // console.log('sending email', email);
 
 
-    // await this.mailService.sendMail({
-    //   to: email.to,
-    //   from: email.from,
-    //   subject: email.subject,
-    //   html: email.html,
-    // });
+    await this.mailService.sendMail({
+      to: email.to,
+      from: email.from,
+      subject: email.subject,
+      html: email.html,
+    });
   }
 }
